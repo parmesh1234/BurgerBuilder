6dc91d0141bf19d02a4439d63f97fe2d3f84e854
parmeshwar <parmesh1042@gmail.com>
2019-01-05 11:54:37 +0530

    Updates

---
+++ precache-manifest.bef6298d7699b9fb2a3073cf4085547d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "954f1eacb51202d94434",
+    "url": "/BurgerBuilder/static/js/runtime~main.954f1eac.js"
+  },
+  {
+    "revision": "a014c3b1ec9c830024e5",
+    "url": "/BurgerBuilder/static/js/main.a014c3b1.chunk.js"
+  },
+  {
+    "revision": "6870da2600cf119d3fed",
+    "url": "/BurgerBuilder/static/js/1.6870da26.chunk.js"
+  },
+  {
+    "revision": "a014c3b1ec9c830024e5",
+    "url": "/BurgerBuilder/static/css/main.51cc801a.chunk.css"
+  },
+  {
+    "revision": "44469f8d77b9d236fb3489ebd1e233f2",
+    "url": "/BurgerBuilder/index.html"
+  }
+];
